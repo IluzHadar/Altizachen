8b4474b96f13e07077286fd24e2342c7ba72657f
iluzhadar <iluzhadar@gmail.com>
2023-03-28 17:32:51 +0300

    add data as array and add images.

--- altizachen/frontend/src/App.js
+++ altizachen/frontend/src/App.js
@@ -1,10 +1,32 @@
+import data from './data';
+
 function App() {
   return (
     <div>
       <header>
-        <a href="/"> Altizachen</a>
+        <a href="/">Altizachen</a>
       </header>
-      <main>list adds</main>
+      <main>
+        <h1>Featured adds</h1>
+        <div className="adds">
+          {data.adds.map((add) => (
+            <div className="add" key={add.slug}>
+              <a href={`/add/${add.slug}`}>
+                <img src={add.image} alt={add.name} />
+              </a>
+              <div className="add-info">
+                <a href={`/add/${add.slug}`}>
+                  <p>{add.name}</p>
+                </a>
+                <p>
+                  <strong>${add.category}</strong>
+                </p>
+                <button>Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
