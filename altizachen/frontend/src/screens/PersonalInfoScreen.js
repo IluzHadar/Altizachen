e548c3e1924a01fb07f4039f8c3d5aaf314df56a
iluzhadar <iluzhadar@gmail.com>
2023-04-29 11:39:32 +0300

    fix the personal info screen card. add colors to buttuns.

--- altizachen/frontend/src/screens/PersonalInfoScreen.js
+++ altizachen/frontend/src/screens/PersonalInfoScreen.js
@@ -17,6 +17,20 @@ const reducer = (state, action) => {
       return { ...state, products: action.payload, loading: false };
     case 'FETCH_FAIL':
       return { ...state, loading: false, erroe: action.payload };
+
+    case 'DELETE_REQUEST':
+      return { ...state, loadingDelete: true, successDelete: false };
+    case 'DELETE_SUCCESS':
+      return {
+        ...state,
+        loadingDelete: false,
+        successDelete: true,
+      };
+    case 'DELETE_FAIL':
+      return { ...state, loadingDelete: false, successDelete: false };
+
+    case 'DELETE_RESET':
+      return { ...state, loadingDelete: false, successDelete: false };
     default:
       return state;
   }
@@ -44,9 +58,21 @@ function PersonalInfoScreen() {
     fetchData();
   }, []);
 
-  const deleteHandler = (id) => {
-    if (window.confirm('Are you sure')) {
-      // DELETE PRODUCTS
+  const deleteHandler = async (id) => {
+    if (window.confirm('Are you sure to delete?')) {
+      console.log('DELETEEEEEEEEEEEEEEEEEEE');
+      try {
+        await axios.delete(`/api/products/${id}`, {
+          headers: { Authorization: `Bearer ${user.token}` },
+        });
+        //toast.success('product deleted successfully');
+        dispatch({ type: 'DELETE_SUCCESS' });
+      } catch (err) {
+        console.log(err);
+        dispatch({
+          type: 'DELETE_FAIL',
+        });
+      }
     }
   };
 
@@ -58,93 +84,84 @@ function PersonalInfoScreen() {
         {products
           .filter((products) => products.numberPhoneUser === user.numberPhone)
           .map((product) => (
-            <div>
-              <Col
-                className="align-items-stretch d-flex"
-                key={product._id}
-                sm={12}
-                md={6}
-                lg={4}
-                xl={3}
-                style={{ padding: '5px' }}
-              >
-                <Card className="w-100 h-80 my-3 p-3 rounded text-center">
-                  <div class="card w-350">
+            <Col
+              className="align-items-stretch d-flex"
+              key={product._id}
+              sm={12}
+              md={6}
+              lg={4}
+              xl={3}
+              style={{ padding: '5px' }}
+            >
+              <Card className="w-100 h-80 my-3 p-3 rounded text-center">
+                <Link
+                  style={{
+                    height: '100%',
+                  }}
+                  to={`/product/${product._id}`}
+                >
+                  <Card.Img
+                    style={{
+                      height: '100%',
+                    }}
+                    src={product.image}
+                    alt={product.name}
+                    variant="top"
+                  />
+                </Link>
+                <Card.Body>
+                  <Link to={`/product/${product._id}`}>
+                    <Card.Title as="div">
+                      <strong>{product.name}</strong>
+                    </Card.Title>
+                  </Link>
+                  <Row>
                     <Link
+                      to={`/editProduct/${product._id}`}
+                      type="button"
+                      class="btn btn-info"
                       style={{
-                        height: '100%',
+                        backgroundColor: 'green',
+                        color: 'black',
+                        width: '64px',
+                        padding: '0px',
                       }}
-                      to={`/product/${product._id}`}
                     >
-                      <Card.Img
-                        style={{
-                          height: '100%',
-                        }}
-                        src={product.image}
-                        alt={product.name}
-                        variant="top"
-                      />
+                      <normal>Edite</normal>
                     </Link>
-                    <Col>
-                      <Card.Body>
-                        <Link to={`/product/${product._id}`}>
-                          <Card.Title as="div">
-                            <strong>{product.name}</strong>
-                          </Card.Title>
-                        </Link>
-                      </Card.Body>
-                    </Col>
-                    <Card.Body>
-                      <Col>
-                        <Link
-                          to={`/EditProduct/${product._id}`}
-                          type="button"
-                          class="btn btn-info"
-                          style={{
-                            color: 'white',
-                            width: '64px',
-                            padding: '0px',
-                          }}
-                        >
-                          <normal>Edite</normal>
-                        </Link>
-                      </Col>
-                      <Col>
-                        <Link
-                          to={`/info`}
-                          variant="danger"
-                          type="button"
-                          class="btn btn-info"
-                          style={{
-                            color: 'white',
-                            width: '64px',
-                            padding: '0px',
-                          }}
-                          onClick={() => deleteHandler(product._id)}
-                        >
-                          <i className="fas fa-trash"></i>
-                          <normal>Delete</normal>
-                        </Link>
-                      </Col>
-                      <Col>
-                        <Link
-                          to={`/product/${product._id}`}
-                          type="button"
-                          class="btn btn-info"
-                          style={{
-                            color: 'white',
-                            width: '64px',
-                            padding: '0px',
-                          }}
-                        >
-                          <normal>Pause</normal>
-                        </Link>
-                      </Col>
-                    </Card.Body>
-                  </div>
-                </Card>
-              </Col>
-            </div>
+
+                    <Link
+                      to={`/info/${product._id}`}
+                      type="button"
+                      class="btn btn-info"
+                      style={{
+                        backgroundColor: 'orange',
+                        color: 'black',
+                        width: '64px',
+                        padding: '0px',
+                      }}
+                      onClick={() => deleteHandler(product._id)}
+                    >
+                      <normal>Delete</normal>
+                    </Link>
+
+                    <Link
+                      to={`/product/${product._id}`}
+                      type="button"
+                      class="btn btn-info"
+                      style={{
+                        backgroundColor: 'yellow',
+                        color: 'black',
+                        width: '64px',
+                        padding: '0px',
+                      }}
+                    >
+                      <normal>Pause</normal>
+                    </Link>
+                  </Row>
+                </Card.Body>
+              </Card>
+            </Col>
           ))}
       </div>
     </div>
