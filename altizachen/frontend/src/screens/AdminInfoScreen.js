1ac985c2047ea3b3bdcd94f07f985ed6ba6b0b1e
iluzhadar <iluzhadar@gmail.com>
2023-05-27 10:45:37 +0300

    add delete logic button in AdminInfoScreen ( delete ad from DB).

--- altizachen/frontend/src/screens/AdminInfoScreen.js
+++ altizachen/frontend/src/screens/AdminInfoScreen.js
@@ -13,8 +13,6 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import MessageBox from '../components/MessageBox';
 
-
-
 const reducer = (state, action) => {
   switch (action.type) {
     case 'FETCH_REQUEST1':
@@ -48,7 +46,6 @@ const reducer = (state, action) => {
   }
 };
 
-
 function AdminInfoScreen() {
   const navigate = useNavigate();
   const [{ products }, dispatch] = useReducer(logger(reducer), {
@@ -62,15 +59,12 @@ function AdminInfoScreen() {
     loading: true,
     error: '',
   });
-  
 
   const [activeLayer, setActiveLayer] = useState(1);
-
 
   const handleButtonClick = (layerNumber) => {
     setActiveLayer(layerNumber);
   };
-
 
   const { state } = useContext(Store);
   const { user } = state;
@@ -88,7 +82,7 @@ function AdminInfoScreen() {
     fetchData();
   }, []);
 
-    useEffect(() => {
+  useEffect(() => {
     const fetchData = async () => {
       dispatch1({ type: 'FETCH_REQUEST1' });
       try {
@@ -101,14 +95,20 @@ function AdminInfoScreen() {
     fetchData();
   }, []);
 
-
   const deleteHandler = async (id) => {
     if (window.confirm('Are you sure to delete?')) {
       try {
         await axios.delete(`/api/products/${id}`, {
           headers: { Authorization: `Bearer ${user.token}` },
         });
+
+        //toast.success('product deleted successfully');
         dispatch({ type: 'DELETE_SUCCESS' });
+        // fetch data again
+        const result = await axios.get('/api/products');
+        dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+
+        // remove from the screen
       } catch (err) {
         console.log(err);
         dispatch({
@@ -118,179 +118,285 @@ function AdminInfoScreen() {
     }
   };
 
-
-
   return (
-    
-      <div>
-          {user &&<h1 style={{ fontWeight: 'bold', textAlign: 'center',padding: '20px' }}>Admin management center</h1>}
-          {user &&
-      
-      <div class="card-body" style={{textAlign: 'center'}}>
-        <div style={{ textAlign: 'center'}}>
-        <Button  style={{ marginRight: '20px' }} onClick={() => handleButtonClick(1)}>Show Ads</Button>
-        <Button style={{ marginLeft: '20px' } }onClick={() => handleButtonClick(2)}>Show Statistic</Button>
-        </div>
+    <div>
+      {user && (
+        <h1
+          style={{ fontWeight: 'bold', textAlign: 'center', padding: '20px' }}
+        >
+          Admin management center
+        </h1>
+      )}
+      {user && (
+        <div class="card-body" style={{ textAlign: 'center' }}>
+          <div style={{ textAlign: 'center' }}>
+            <Button
+              style={{ marginRight: '20px' }}
+              onClick={() => handleButtonClick(1)}
+            >
+              Show Ads
+            </Button>
+            <Button
+              style={{ marginLeft: '20px' }}
+              onClick={() => handleButtonClick(2)}
+            >
+              Show Statistic
+            </Button>
+          </div>
 
-
-        <div style={{padding:'20px'}}>
-        {activeLayer === 1 && (
-          
-         
-          <div style={{ backgroundColor: 'lightgray', padding: '20px', marginTop: '10px', borderRadius: '10px', textAlign: 'center' }}>
-            <h2 style={{color:'white',  fontWeight: 'bold', textDecoration: 'underline'}}>List of Ads in the website </h2>
-            <div className = 'products'>
-
-              {user && products.map((product) => (
-                  <Col
-                    className="align-items-stretch d-flex"
-                    key={product._id}
-                    sm={12}
-                    md={6}
-                    lg={4}
-                    xl={4}
-                    style={{ padding: '20px' }}
-                  >
-                    <Card className="w-100 h-80 my-3 p-3 rounded text-center">
-                      <Link
-                        style={{
-                          height: '100%',
-                        }}
-                        to={`/product/${product._id}`}
+          <div style={{ padding: '20px' }}>
+            {activeLayer === 1 && (
+              <div
+                style={{
+                  backgroundColor: 'lightgray',
+                  padding: '20px',
+                  marginTop: '10px',
+                  borderRadius: '10px',
+                  textAlign: 'center',
+                }}
+              >
+                <h2
+                  style={{
+                    color: 'white',
+                    fontWeight: 'bold',
+                    textDecoration: 'underline',
+                  }}
+                >
+                  List of Ads in the website{' '}
+                </h2>
+                <div className="products">
+                  {user &&
+                    products.map((product) => (
+                      <Col
+                        className="align-items-stretch d-flex"
+                        key={product._id}
+                        sm={12}
+                        md={6}
+                        lg={4}
+                        xl={4}
+                        style={{ padding: '20px' }}
                       >
-                        <Card.Img
-                          style={{
-                            height: '100%',
-                          }}
-                          src={product.image}
-                          alt={product.name}
-                          variant="top"
-                        />
-                      </Link>
-                      <Card.Body >
-                        <Link to={`/product/${product._id}`}>
-                          <Card.Title as="div">
-                            <strong>{product.name}</strong>
-                            
-                          </Card.Title>
-                        </Link>
-                        <span style={{fontWeight: 'bold'}}>Owner: </span>{product.OwnerName}
-                        <Row>
-                          <Button
-                            variant="outline-danger"
-                            to={`/info/${product._id}`}
-                            class="btn btn-info"
+                        <Card className="w-100 h-80 my-3 p-3 rounded text-center">
+                          <Link
                             style={{
-                              padding: '0px',
-                              positionAlign: 'center'
+                              height: '100%',
                             }}
-                            onClick={() => deleteHandler(product._id)}
+                            to={`/product/${product._id}`}
                           >
-                            Delete
-                          </Button>
-                        </Row>
-                      </Card.Body>
-                    </Card>
+                            <Card.Img
+                              style={{
+                                height: '100%',
+                              }}
+                              src={product.image}
+                              alt={product.name}
+                              variant="top"
+                            />
+                          </Link>
+                          <Card.Body>
+                            <Link to={`/product/${product._id}`}>
+                              <Card.Title as="div">
+                                <strong>{product.name}</strong>
+                              </Card.Title>
+                            </Link>
+                            <span style={{ fontWeight: 'bold' }}>Owner: </span>
+                            {product.OwnerName}
+                            <Row>
+                              <Button
+                                variant="outline-danger"
+                                to={`/info/${product._id}`}
+                                class="btn btn-info"
+                                style={{
+                                  padding: '0px 10px 0px 10px',
+                                  positionAlign: 'center',
+                                }}
+                                onClick={() => deleteHandler(product._id)}
+                              >
+                                Delete
+                              </Button>
+                            </Row>
+                          </Card.Body>
+                        </Card>
+                      </Col>
+                    ))}
+                </div>
+              </div>
+            )}
+            {activeLayer === 2 && (
+              <div
+                style={{
+                  backgroundColor: 'lightgray',
+                  padding: '20px',
+                  marginTop: '10px',
+                  borderRadius: '10px',
+                  textAlign: 'center',
+                }}
+              >
+                <h2
+                  style={{
+                    color: 'white',
+                    fontWeight: 'bold',
+                    textDecoration: 'underline',
+                  }}
+                >
+                  Statistic{' '}
+                </h2>
+                <Row>
+                  <Col style={{ textAlign: 'left' }}>
+                    <div>
+                      The amount of ads in Forniture caregory:
+                      {
+                        products
+                          .filter((products) => products.category === 1)
+                          .map((product) => product).length
+                      }
+                    </div>
+                    <br></br>
+                    <div>
+                      The amount of ads in Electrical products caregory:
+                      {
+                        products
+                          .filter((products) => products.category === 2)
+                          .map((product) => product).length
+                      }
+                    </div>
+                    <br></br>
+                    <div>
+                      The amount of ads in Home Products caregory:
+                      {
+                        products
+                          .filter((products) => products.category === 3)
+                          .map((product) => product).length
+                      }
+                    </div>
+                    <br></br>
+                    <div>
+                      The amount of ads in Garden products caregory:
+                      {
+                        products
+                          .filter((products) => products.category === 4)
+                          .map((product) => product).length
+                      }
+                    </div>
+                    <br></br>
+                    <div>
+                      The amount of ads in Car Products caregory:
+                      {
+                        products
+                          .filter((products) => products.category === 5)
+                          .map((product) => product).length
+                      }
+                    </div>
+                    <br></br>
+                    <div>
+                      The amount of ads in Animel Products caregory:
+                      {
+                        products
+                          .filter((products) => products.category === 6)
+                          .map((product) => product).length
+                      }
+                    </div>
+                    <br></br>
+                    <div>
+                      The amount of ads in Forniture caregory:
+                      {
+                        products
+                          .filter((products) => products.category === 1)
+                          .map((product) => product).length
+                      }
+                    </div>
+                    <br></br>
+                    <div>
+                      The amount of ads in Electrical products caregory:
+                      {
+                        products
+                          .filter((products) => products.category === 2)
+                          .map((product) => product).length
+                      }
+                    </div>
+                    <br></br>
+                    <div>
+                      The amount of ads in Home Products caregory:
+                      {
+                        products
+                          .filter((products) => products.category === 3)
+                          .map((product) => product).length
+                      }
+                    </div>
+                    <br></br>
+                    <div>
+                      The amount of ads in Garden products caregory:
+                      {
+                        products
+                          .filter((products) => products.category === 4)
+                          .map((product) => product).length
+                      }
+                    </div>
+                    <br></br>
+                    <div>
+                      The amount of ads in Car Products caregory:
+                      {
+                        products
+                          .filter((products) => products.category === 5)
+                          .map((product) => product).length
+                      }
+                    </div>
+                    <br></br>
+                    <div>
+                      The amount of ads in status pause:
+                      {
+                        products
+                          .filter((products) => products.pauseAd === true)
+                          .map((product) => product).length
+                      }
+                    </div>
+                    <br></br>
+                    <div>
+                      The amount of ads in the website:
+                      {
+                        products
+                          .filter((products) => products)
+                          .map((product) => product).length
+                      }
+                    </div>
+                    <br></br>
+                    <div>
+                      The amount of users in the website:
+                      {
+                        users.filter((users) => users).map((user) => user)
+                          .length
+                      }
+                    </div>
                   </Col>
-                ))}
-                
-            </div>
+                  <Col style={{ textAlign: 'left' }}>
+                    <div>
+                      <Link
+                        to={`/product/${
+                          products.reduce((maxProd, currprod) =>
+                            currprod.like > maxProd.like ? currprod : maxProd
+                          )._id
+                        }`}
+                      >
+                        The ad with the most likes{' '}
+                      </Link>
+                    </div>
+                    <br></br>
+                    <div>
+                      The highest number of likes on an ad:
+                      {products.reduce(
+                        (max, product) =>
+                          product.like > max ? product.like : max,
+                        0
+                      )}
+                    </div>
+                  </Col>
+                </Row>
+              </div>
+            )}
           </div>
-        )}
-        {activeLayer === 2 && (
-          <div style={{ backgroundColor: 'lightgray', padding: '20px', marginTop: '10px',  borderRadius: '10px', textAlign: 'center' }}>
-            <h2 style={{color:'white',  fontWeight: 'bold', textDecoration: 'underline'}}>Statistic   </h2>
-        <Row>
-          <Col style={{textAlign: 'left'}} >
-          <div>The amount of ads in Forniture caregory: 
-           {products.filter((products) =>products.category === 1 )
-            .map((product) => ( product)).length}
-          </div>
-          <br></br>
-          <div>The amount of ads in Electrical products caregory: 
-           {products.filter((products) =>products.category === 2 )
-            .map((product) => ( product)).length}
-          </div><br></br>
-          <div>The amount of ads in Home Products caregory: 
-           {products.filter((products) =>products.category === 3 )
-            .map((product) => ( product)).length}
-          </div><br></br>
-          <div>The amount of ads in Garden products caregory: 
-           {products.filter((products) =>products.category === 4 )
-            .map((product) => ( product)).length}
-          </div><br></br>
-          <div>The amount of ads in Car Products caregory: 
-           {products.filter((products) =>products.category === 5 )
-            .map((product) => ( product)).length}
-          </div><br></br>
-          <div>The amount of ads in Animel Products caregory: 
-           {products.filter((products) =>products.category === 6 )
-            .map((product) => ( product)).length}
-          </div><br></br>
-          <div>The amount of ads in Forniture caregory: 
-           {products.filter((products) =>products.category === 1 )
-            .map((product) => ( product)).length}
-          </div><br></br>
-          <div>The amount of ads in Electrical products caregory: 
-           {products.filter((products) =>products.category === 2 )
-            .map((product) => ( product)).length}
-          </div><br></br>
-          <div>The amount of ads in Home Products caregory: 
-           {products.filter((products) =>products.category === 3 )
-            .map((product) => ( product)).length}
-          </div><br></br>
-          <div>The amount of ads in Garden products caregory: 
-           {products.filter((products) =>products.category === 4 )
-            .map((product) => ( product)).length}
-          </div><br></br>
-          <div>The amount of ads in Car Products caregory: 
-           {products.filter((products) =>products.category === 5 )
-            .map((product) => ( product)).length}
-          </div><br></br>
-          <div>The amount of ads in status pause: 
-           {products.filter((products) =>products.pauseAd === true )
-            .map((product) => ( product)).length}
-          </div><br></br>
-          <div>The amount of ads in the website: 
-           {products.filter((products) =>products )
-            .map((product) => ( product)).length}
-          </div><br></br>
-          <div>The amount of users in the website: 
-           {users.filter((users) =>users )
-            .map((user) => ( user)).length}
-          </div>
-
-          </Col>
-          <Col style={{textAlign: 'left'}} >
-            <div><Link to={`/product/${(products.reduce((maxProd, currprod) => (currprod.like > maxProd.like ? currprod : maxProd)))._id}`}>The ad with the most likes </Link>
-            </div><br></br>
-            <div>The highest number of likes on an ad: 
-            {products.reduce((max, product) => (product.like > max ? product.like : max), 0)}
-            </div>
-
-            
-
-          </Col>
-        </Row>
-          
-          
-          
-          
-          
-          
-          </div>
-
-            
-
-        )}
-      </div>
         </div>
-     }
-      
-      </div>
-      
-  
+      )}
+    </div>
   );
-};
+}
 
 export default AdminInfoScreen;
--- altizachen/frontend/src/screens/PersonalInfoScreen.js
+++ altizachen/frontend/src/screens/PersonalInfoScreen.js
@@ -1,115 +1,101 @@
-import { useNavigate } from 'react-router-dom'
-import { useEffect, useReducer } from 'react'
-import { Store } from '../Store'
-import { Link } from 'react-router-dom'
-import axios from 'axios'
-import logger from 'use-reducer-logger'
-import Col from 'react-bootstrap/Col'
-import Card from 'react-bootstrap/Card'
-import React, { useState, useContext } from 'react'
-import { Row } from 'react-bootstrap'
-import { right } from '@popperjs/core'
-import Button from 'react-bootstrap/Button'
-import Form from 'react-bootstrap/Form'
-import MessageBox from '../components/MessageBox'
+import { useNavigate } from 'react-router-dom';
+import { useEffect, useReducer } from 'react';
+import { Store } from '../Store';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import logger from 'use-reducer-logger';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import React, { useState, useContext } from 'react';
+import { Row } from 'react-bootstrap';
+import { right } from '@popperjs/core';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import MessageBox from '../components/MessageBox';
 
 const reducer = (state, action) => {
   switch (action.type) {
     case 'FETCH_REQUEST':
-      return { ...state, loading: true }
+      return { ...state, loading: true };
     case 'FETCH_SUCCESS':
-      return { ...state, products: action.payload, loading: false }
+      return { ...state, products: action.payload, loading: false };
     case 'FETCH_FAIL':
-      return { ...state, loading: false, erroe: action.payload }
+      return { ...state, loading: false, erroe: action.payload };
 
     case 'DELETE_REQUEST':
-      return { ...state, loadingDelete: true, successDelete: false }
+      return { ...state, loadingDelete: true, successDelete: false };
     case 'DELETE_SUCCESS':
       return {
         ...state,
         loadingDelete: false,
         successDelete: true,
-      }
+      };
     case 'DELETE_FAIL':
-      return { ...state, loadingDelete: false, successDelete: false }
+      return { ...state, loadingDelete: false, successDelete: false };
 
     case 'DELETE_RESET':
-      return { ...state, loadingDelete: false, successDelete: false }
+      return { ...state, loadingDelete: false, successDelete: false };
     default:
-      return state
+      return state;
   }
-}
+};
 
 function PersonalInfoScreen() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const [{ products }, dispatch] = useReducer(logger(reducer), {
     products: [],
     loading: true,
     error: '',
-  })
+  });
 
-  const { state } = useContext(Store)
-  const { user } = state
+  const { state } = useContext(Store);
+  const { user } = state;
 
   useEffect(() => {
     const fetchData = async () => {
-      dispatch({ type: 'FETCH_REQUEST' })
+      dispatch({ type: 'FETCH_REQUEST' });
       try {
-        const result = await axios.get('/api/products')
-        dispatch({ type: 'FETCH_SUCCESS', payload: result.data })
+        const result = await axios.get('/api/products');
+        dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
       } catch (err) {
-        dispatch({ type: 'FETCH_FAIL', payload: err.message })
+        dispatch({ type: 'FETCH_FAIL', payload: err.message });
       }
-    }
-    fetchData()
-  }, [])
+    };
+    fetchData();
+  }, []);
 
   const deleteHandler = async (id) => {
     if (window.confirm('Are you sure to delete?')) {
       try {
         await axios.delete(`/api/products/${id}`, {
           headers: { Authorization: `Bearer ${user.token}` },
-        })
+        });
 
         //toast.success('product deleted successfully');
-        dispatch({ type: 'DELETE_SUCCESS' })
+        dispatch({ type: 'DELETE_SUCCESS' });
         // fetch data again
-        const result = await axios.get('/api/products')
-        dispatch({ type: 'FETCH_SUCCESS', payload: result.data })
+        const result = await axios.get('/api/products');
+        dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
 
         // remove from the screen
       } catch (err) {
-        console.log(err)
+        console.log(err);
         dispatch({
           type: 'DELETE_FAIL',
-        })
+        });
       }
     }
-  }
-
-  // const submitHandler = async (e) => {
-  //   e.preventDefault();
-  //   try {
-  //     console.log('PAUSEEEEEEEE');
-  //     products.pauseAd = true;
-  //     const { data2 } = await axios.put(
-  //       `/api/products/${products._id}`,
-  //       products
-  //     );
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
+  };
 
   return (
-    <div class='card-body'>
+    <div class="card-body">
       {user && (
         <h1 style={{ fontWeight: 'bold', textAlign: 'center' }}>
           Personal Info
         </h1>
       )}
       {user && (
-        <Card className='w-100 h-80 my-3 p-3 rounded text-center'>
+        <Card className="w-100 h-80 my-3 p-3 rounded text-center">
           <Card.Body>
             <h7>
               Hello, {user.name}
@@ -126,13 +112,13 @@ function PersonalInfoScreen() {
         </Card>
       )}
 
-      <div className='products'>
+      <div className="products">
         {user &&
           products
             .filter((products) => products.numberPhoneUser === user.numberPhone)
             .map((product) => (
               <Col
-                className='align-items-stretch d-flex'
+                className="align-items-stretch d-flex"
                 key={product._id}
                 sm={12}
                 md={6}
@@ -140,7 +126,7 @@ function PersonalInfoScreen() {
                 xl={3}
                 style={{ padding: '5px' }}
               >
-                <Card className='w-100 h-80 my-3 p-3 rounded text-center'>
+                <Card className="w-100 h-80 my-3 p-3 rounded text-center">
                   <Link
                     style={{
                       height: '100%',
@@ -153,12 +139,12 @@ function PersonalInfoScreen() {
                       }}
                       src={product.image}
                       alt={product.name}
-                      variant='top'
+                      variant="top"
                     />
                   </Link>
                   <Card.Body>
                     <Link to={`/product/${product._id}`}>
-                      <Card.Title as='div'>
+                      <Card.Title as="div">
                         <strong>{product.name}</strong>
                       </Card.Title>
                     </Link>
@@ -166,9 +152,9 @@ function PersonalInfoScreen() {
                     <Row>
                       <Col>
                         <Link
-                          variant='outline-info'
+                          variant="outline-info"
                           to={`/EditProduct/${product._id}`}
-                          class='btn btn-info'
+                          class="btn btn-info"
                           style={{
                             padding: '0px 20px 0px 20px',
                             positionAlign: 'center',
@@ -179,9 +165,9 @@ function PersonalInfoScreen() {
                       </Col>
                       <Col>
                         <Button
-                          variant='outline-danger'
+                          variant="outline-danger"
                           to={`/info/${product._id}`}
-                          class='btn btn-info'
+                          class="btn btn-info"
                           style={{
                             padding: '0px 10px 0px 10px',
                             positionAlign: 'center',
@@ -198,7 +184,7 @@ function PersonalInfoScreen() {
             ))}
       </div>
     </div>
-  )
+  );
 }
 
-export default PersonalInfoScreen
+export default PersonalInfoScreen;
